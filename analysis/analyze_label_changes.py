624e770bfaf1eb60e93d1b98ccc9f7c3782a2df6
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2021-10-08 14:57:43 +0000

    Improved comment

--- analysis/analyze_label_changes.py
+++ analysis/analyze_label_changes.py
@@ -2,4 +2,11 @@ import os.path as osp
 import json
 import argparse
 import numpy as np
-import pandas as p
+import pandas as pd
+
+def resolve_classes(class_set, index2class):
+    return [index2class[c] for c in list(class_set)]
+
+
+def main():
+    parser = argparse.Ar
