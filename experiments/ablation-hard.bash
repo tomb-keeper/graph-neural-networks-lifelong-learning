7d9a2bffafafa6456881e09fe4623f8f0653e975
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-01-10 12:54:12 +0000

    Update Config.java

--- experiments/ablation-hard.bash
+++ experiments/ablation-hard.bash
@@ -1,4 +1,6 @@
 DATA="dblp-hard"
 YEAR=2004
 HISTORY=3
-NLAYERS=1 # 2 graph conv layer
+NLAYERS=1 # 2 graph conv layers
+ARGS="--start warm --n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --history $HISTORY --rescale_lr 1. --rescale_wd 1."
+DATA_ARGS="--data "$DATA" --t_st
