58c258a244b3e5cf039b3dfc7af458699eddce5f
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-01-12 20:27:31 +0000

    jekyll build

--- experiments/ablation-hard.bash
+++ experiments/ablation-hard.bash
@@ -9,4 +9,5 @@ UPTRAIN_MODEL_ARGS="--initial_epochs 0 --annual_epochs 200"
 OUTFILE="results/ablation-hard.csv"
 
 for SEED in 1 2 3 4 5 6 7 8 9 10; do
-  python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64 --lr "0.001" $STATIC
+  python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64 --lr "0.001" $STATIC_MODEL_ARGS $ARGS $DATA_ARGS --save "$OUTFILE"
+  python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64
