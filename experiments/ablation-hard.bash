7c12b92e69d0bda7ee10f6809bb7b18bd97058e9
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-01-12 16:41:39 +0000

    net: fib: fix incorrect call_rcu_bh()

--- experiments/ablation-hard.bash
+++ experiments/ablation-hard.bash
@@ -3,4 +3,7 @@ YEAR=2004
 HISTORY=3
 NLAYERS=1 # 2 graph conv layers
 ARGS="--start warm --n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --history $HISTORY --rescale_lr 1. --rescale_wd 1."
-DATA_ARGS="--data "$DATA" --t_st
+DATA_ARGS="--data "$DATA" --t_start $YEAR"
+STATIC_MODEL_ARGS="--initial_epochs 400 --annual_epochs 0"
+UPTRAIN_MODEL_ARGS="--initial_epochs 0 --annual_epochs 200"
+OUTFIL
