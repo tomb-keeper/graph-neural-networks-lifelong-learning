2b3e9447eebdac20e406e55969b69c85adea5ab6
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-01-28 09:27:50 +0000

    Missing newline

--- experiments/ablation-hpopt.bash
+++ experiments/ablation-hpopt.bash
@@ -2,4 +2,6 @@ DATA="dblp-easy"
 YEAR=2004
 HISTORY=3
 NLAYERS=1 # 2 graph conv layers
-ARGS="--start warm --n_layers $NLAYE
+ARGS="--start warm --n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --history $HISTORY --rescale_wd 1. --rescale_lr 1."
+DATA_ARGS="--data "$DATA" --t_start $YEAR"
+STATIC
