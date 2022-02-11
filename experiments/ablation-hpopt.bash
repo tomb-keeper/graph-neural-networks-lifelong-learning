42bd71260c8f2e5216014e31dbfb4620ea815a62
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-02-11 17:19:28 +0000

    One more User Story done

--- experiments/ablation-hpopt.bash
+++ experiments/ablation-hpopt.bash
@@ -4,4 +4,6 @@ HISTORY=3
 NLAYERS=1 # 2 graph conv layers
 ARGS="--start warm --n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --history $HISTORY --rescale_wd 1. --rescale_lr 1."
 DATA_ARGS="--data "$DATA" --t_start $YEAR"
-STATIC
+STATIC_MODEL_ARGS="--initial_epochs 400 --annual_epochs 0"
+UPTRAIN_MODEL_ARGS="--initial_epochs 0 --annual_epochs 200"
+OUTFILE="result
