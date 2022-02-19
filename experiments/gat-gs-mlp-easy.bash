f028002d015f139676e8c0f370a35091ddcdfd28
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-02-19 23:06:54 +0000

    Update more.html

--- experiments/gat-gs-mlp-easy.bash
+++ experiments/gat-gs-mlp-easy.bash
@@ -5,4 +5,8 @@ ANNUAL_EPOCHS=200
 NLAYERS=1
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS --backend dgl"
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
-OUTFILE="result
+OUTFILE="results/gat-gs-mlp-easy.csv"
+
+for SEED in 1 2 3 4 5 6 7 8 9 10; do
+  # HISTORY 1
+  python3 run_experiment_new.py --seed "$SEED" --model mlp --n_hidden 64 -
