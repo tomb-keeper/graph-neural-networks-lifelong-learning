2068acb41a273b46313eb2f96b687cd1a83110f2
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-02-20 17:28:54 +0000

    Add i18n support

--- experiments/gat-gs-mlp-easy.bash
+++ experiments/gat-gs-mlp-easy.bash
@@ -10,4 +10,5 @@ OUTFILE="results/gat-gs-mlp-easy.csv"
 for SEED in 1 2 3 4 5 6 7 8 9 10; do
   # HISTORY 1
   python3 run_experiment_new.py --seed "$SEED" --model mlp --n_hidden 64 --start cold --lr "0.001" --history 1 $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
-  python3 run_experiment_new.py --seed "$SEED" --mode
+  python3 run_experiment_new.py --seed "$SEED" --model mlp --n_hidden 64 --start warm --lr "0.001" --history 1 $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
+  python3 run_experiment_new.py --seed "$SEED" --mo
