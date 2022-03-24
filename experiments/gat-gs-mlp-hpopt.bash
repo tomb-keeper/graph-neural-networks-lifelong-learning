769948688836c0765b3d437e0a9c4b00db243ac3
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-03-24 18:29:22 +0000

    Changed homepage

--- experiments/gat-gs-mlp-hpopt.bash
+++ experiments/gat-gs-mlp-hpopt.bash
@@ -3,4 +3,5 @@ YEAR=2004
 INITIAL_EPOCHS=0
 ANNUAL_EPOCHS=200
 NLAYERS=1
-ARGS="--n_layers $NLAYERS --weight_dec
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS"
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITI
