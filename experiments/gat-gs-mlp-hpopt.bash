1d73b6533525742a54cdebd38daf30bae5422898
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-04-02 20:03:11 +0000

    Fixes #75

--- experiments/gat-gs-mlp-hpopt.bash
+++ experiments/gat-gs-mlp-hpopt.bash
@@ -4,4 +4,8 @@ INITIAL_EPOCHS=0
 ANNUAL_EPOCHS=200
 NLAYERS=1
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS"
-PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITI
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+OUTFILE="results/gat-gs-mlp-hpopt.csv"
+
+for SEED in 101 102 103; do
+  for LR in "0.1" "0.05" "0.01" "0.005" "0.001" "0.0005";
