988854044985cb841e2c8a3ff7b012a8c0326cdb
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-04-15 16:45:38 +0000

    Change log updates

--- experiments/gat-gs-mlp-hpopt.bash
+++ experiments/gat-gs-mlp-hpopt.bash
@@ -12,4 +12,5 @@ for SEED in 101 102 103; do
     for HISTORY in 1 3 6 25; do
       python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64 --start warm --lr $LR --history $HISTORY $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
       python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64 --start cold --lr $LR --history $HISTORY $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
-      python3 run_experiment.py --seed "$SEED" --model gs-mean --n_hidden 
+      python3 run_experiment.py --seed "$SEED" --model gs-mean --n_hidden 32 --start warm --lr $LR --history $HISTORY $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
+      python3 run_experiment.py --seed "$SEED" -
