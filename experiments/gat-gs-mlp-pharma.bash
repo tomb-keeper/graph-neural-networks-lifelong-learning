f0c60a81088d61e7d49273fd50af74ed8782b921
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-04-19 15:19:24 +0000

    ARM: optimize memset_io()/memcpy_fromio()/memcpy_toio()

--- experiments/gat-gs-mlp-pharma.bash
+++ experiments/gat-gs-mlp-pharma.bash
@@ -4,4 +4,9 @@ INITIAL_EPOCHS=0
 ANNUAL_EPOCHS=200
 NLAYERS=1
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS"
-PRETRAIN_ARGS="--t_start $YEAR 
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+OUTFILE="results/gat-gs-mlp-pharma.csv"
+
+for SEED in 1 2 3 4 5 6 7 8 9 10; do
+  # HISTORY 1
+  python3 run_experiment.py --seed 
