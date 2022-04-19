3613b3f9f8f6eccb98f9100d7593ef9b9dd73ef5
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-04-19 22:30:28 +0000

    change front end layout

--- experiments/gat-gs-mlp-pharma.bash
+++ experiments/gat-gs-mlp-pharma.bash
@@ -9,4 +9,5 @@ OUTFILE="results/gat-gs-mlp-pharma.csv"
 
 for SEED in 1 2 3 4 5 6 7 8 9 10; do
   # HISTORY 1
-  python3 run_experiment.py --seed 
+  python3 run_experiment.py --seed "$SEED" --model mlp --n_hidden 64 --start cold --lr "0.001" --history 1 $ARGS $PRETRAIN_ARGS --dataset "$DATA" --save "$OUTFILE"
+  pyt
