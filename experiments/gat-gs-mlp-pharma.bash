027135b01073bbf6ff51e8483cdd8d17743d08f4
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-04-19 14:29:46 +0000

    Update tasks.py

--- experiments/gat-gs-mlp-pharma.bash
+++ experiments/gat-gs-mlp-pharma.bash
@@ -3,4 +3,5 @@ YEAR=1999
 INITIAL_EPOCHS=0
 ANNUAL_EPOCHS=200
 NLAYERS=1
-ARG
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS"
+PRETRAIN_ARGS="--t_start $YEAR 
