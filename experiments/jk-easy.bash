3b096596ed18eb3edab3e9f71c93aa675bbafb4e
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-06 12:20:25 +0000

    fixed #35

--- experiments/jk-easy.bash
+++ experiments/jk-easy.bash
@@ -4,4 +4,5 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
 OUTFILE="results/jk-easy.csv"
-ARGS="-
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16"
+PRETRAIN_ARGS="--t_start $YEAR --initi
