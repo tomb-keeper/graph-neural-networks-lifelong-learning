2f98208c3998cbc9b8f354af2ec5eaf5895e2520
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-20 22:01:42 +0000

    msm: kgsl: Fix nice level for higher priority GPU start thread

--- experiments/jk-hpopt.bash
+++ experiments/jk-hpopt.bash
@@ -1,2 +1,8 @@
 # origin: ../experiments-journal/jk-hpopt3layers.bash but changed LAYERS=2
-D
+DATA="dblp-easy"
+YEAR=2004
+INITIAL_EPOCHS=200
+ANNUAL_EPOCHS=200
+NLAYERS=2
+OUTFILE="results/jk-hpopt.csv"
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 
