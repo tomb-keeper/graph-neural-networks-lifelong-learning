b30f1e9f2395d3d57bae68a210d8ce8da615e2be
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-20 13:10:48 +0000

    sched,rt: fix isolated CPUs leaving root_task_group indefinitely throttled

--- experiments/jk-hard.bash
+++ experiments/jk-hard.bash
@@ -1,9 +1,10 @@
-DATA="dblp-easy"
+
+DATA="dblp-hard"
 YEAR=2004
 INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
-OUTFILE="results/jk-easy.csv"
+OUTFILE="results/jk-hard.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16"
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
 hparams=("--history 1 --start cold --lr 0.005"
---
+++ experiments/jk-hpopt.bash
@@ -0,0 +1,2 @@
+# origin: ../experiments-journal/jk-hpopt3layers.bash but changed LAYERS=2
+D
