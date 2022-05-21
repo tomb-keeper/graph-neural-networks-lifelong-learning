b1784896c450b724c9e195a291d5f91d31281bcd
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-21 10:06:01 +0000

    code style cleanup

--- experiments/jk-hpopt.bash
+++ experiments/jk-hpopt.bash
@@ -5,4 +5,10 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
 OUTFILE="results/jk-hpopt.csv"
-ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+HIDDEN=16
+
+set -e
+
+for LR in "0.1" "0
