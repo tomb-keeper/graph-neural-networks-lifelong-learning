cdefd994bb238cb06f05ef8ca239f88e6fc7394d
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-21 15:39:05 +0000

    timing

--- experiments/jk-hpopt.bash
+++ experiments/jk-hpopt.bash
@@ -15,4 +15,4 @@ for LR in "0.1" "0.05" "0.01" "0.005" "0.001" "0.0005"; do
 	for START in "cold" "warm"; do
 		for HISTORY in 1 3 6 25; do
 			for SEED in 101 102 103; do
-				pyth
+				python3 run_experiment_new.py --history $HISTORY --seed "$SEED" --backend dgl --model jknet-sageconv --n_hidden $HIDDEN --start $START --lr 
