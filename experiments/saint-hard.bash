a529c74c9b265e4242f2afeb94164587f8249e1d
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-05-30 21:45:06 +0000

    whops

--- experiments/saint-hard.bash
+++ experiments/saint-hard.bash
@@ -4,4 +4,5 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
 OUTFILE="results/saint-hard.csv"
-ARGS="--n_layers $NLAYE
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16 --sampling rw --batch_size 0.5 --saint_coverage 0"
+PRETRAIN_ARGS="--t_start $YEA
