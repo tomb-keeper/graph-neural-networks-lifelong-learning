e63fa5809ccbfdbc0fe4cc8a3c99e3455c730cec
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-01 09:15:00 +0000

    Remove useless log

--- experiments/saint-hard.bash
+++ experiments/saint-hard.bash
@@ -5,4 +5,7 @@ ANNUAL_EPOCHS=200
 NLAYERS=2
 OUTFILE="results/saint-hard.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16 --sampling rw --batch_size 0.5 --saint_coverage 0"
-PRETRAIN_ARGS="--t_start $YEA
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+hparams=("--history 1 --start cold --lr 0.005"
+            "--history 3 --start cold --lr 0.005"
+            "
