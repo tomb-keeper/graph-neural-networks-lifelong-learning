85490f22c48f7bf0f7a1cc3bb074bc89839ab393
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-04 22:51:46 +0000

    Fix #176

--- experiments/saint-hpopt.bash
+++ experiments/saint-hpopt.bash
@@ -1,1 +1,7 @@
-# origin: ../experiments-journal/saint-hpo
+# origin: ../experiments-journal/saint-hpopt3layers.bash but changed LAYERS=2
+DATA="dblp-easy"
+YEAR=2004
+INITIAL_EPOCHS=200
+ANNUAL_EPOCHS=200
+NLAYERS=2
+OUTFILE="results/saint-hpop
