71e2d2bdc2df89c7084c4ac4dbadb4040f2205cb
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-05 13:43:20 +0000

    Scalafmt

--- experiments/saint-hpopt.bash
+++ experiments/saint-hpopt.bash
@@ -4,4 +4,6 @@ YEAR=2004
 INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
-OUTFILE="results/saint-hpop
+OUTFILE="results/saint-hpopt.csv"
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --sampling rw --saint_coverage 0"
+PRETRAIN_ARG
