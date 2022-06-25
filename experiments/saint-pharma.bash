bd3a4b183b0e44fbc820279c220a2be782d7b6dc
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-25 18:58:14 +0000

    Bump nokogiri from 1.10.3 to 1.10.4

--- experiments/saint-pharma.bash
+++ experiments/saint-pharma.bash
@@ -4,4 +4,6 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2
 OUTFILE="results/saint-pharma.csv"
-ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_l
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16 --sampling rw --saint_coverage 0"
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+hpara
