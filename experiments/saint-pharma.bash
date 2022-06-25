ddebe12d93ab1ab0c751bb850220eecdb0edf408
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-25 18:44:30 +0000

    contact us

--- experiments/saint-pharma.bash
+++ experiments/saint-pharma.bash
@@ -1,1 +1,7 @@
-DATA="pha
+DATA="pharmabio"
+YEAR=1999
+INITIAL_EPOCHS=200
+ANNUAL_EPOCHS=200
+NLAYERS=2
+OUTFILE="results/saint-pharma.csv"
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_l
