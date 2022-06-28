e63849e0efcd541b05e907f9ee9fe11598b286c5
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-06-28 19:17:05 +0000

    extend test

--- experiments/saint-pharma.bash
+++ experiments/saint-pharma.bash
@@ -6,4 +6,7 @@ NLAYERS=2
 OUTFILE="results/saint-pharma.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1. --n_hidden 16 --sampling rw --saint_coverage 0"
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
-hpara
+hparams=(
+	"--history 1 --start cold --lr 0.005 --batch_size 0.5"
+	"--history 4 --start cold --lr 0.005 --batch_size 0.5"
+	"--hist
