f194aab1c9bffea0d501d3853f08732ffa263966
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-07-16 12:36:15 +0000

    fix lang

--- experiments/sg-easy.bash
+++ experiments/sg-easy.bash
@@ -4,4 +4,7 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2  # K hops in SGNet
 OUTFILE="results/sg-easy.csv"
-ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+hparams=("--history 1 --start cold --lr 0.005"
+    
