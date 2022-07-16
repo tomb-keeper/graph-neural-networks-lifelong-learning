cc1cba18302e92f394a04c17f729c539bbe97771
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-07-16 16:19:48 +0000

    :lollipop: Added .gitattributes & .gitignore files

--- experiments/sg-easy.bash
+++ experiments/sg-easy.bash
@@ -7,4 +7,6 @@ OUTFILE="results/sg-easy.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
 hparams=("--history 1 --start cold --lr 0.005"
-    
+            "--history 3 --start cold --lr 0.01"
+            "--history 6 --start cold --lr 0.01"
+            "--history 25 --start cold --
