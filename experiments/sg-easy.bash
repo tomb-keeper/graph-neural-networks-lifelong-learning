9b971df2efad2f6349d94eccd33489bbd3d7c935
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-07-16 08:24:41 +0000

    Updating language files.

--- experiments/sg-easy.bash
+++ experiments/sg-easy.bash
@@ -1,3 +1,7 @@
 DATA="dblp-easy"
 YEAR=2004
-INITIAL_EPOCHS
+INITIAL_EPOCHS=200
+ANNUAL_EPOCHS=200
+NLAYERS=2  # K hops in SGNet
+OUTFILE="results/sg-easy.csv"
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 
