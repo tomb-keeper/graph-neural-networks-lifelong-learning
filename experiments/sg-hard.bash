2c36d58793e254d6e18eb7631376c00bf1616c5d
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-08-01 22:49:26 +0000

    Worked on source generate script

--- experiments/sg-hard.bash
+++ experiments/sg-hard.bash
@@ -1,9 +1,10 @@
-DATA="dblp-easy"
+
+DATA="dblp-hard"
 YEAR=2004
 INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2  # K hops in SGNet
-OUTFILE="results/sg-easy.csv"
+OUTFILE="results/sg-hard.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
 hparams=("--history 1 --start cold --lr 0.005"
