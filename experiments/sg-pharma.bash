cb5ff144feedb580fff903185a421fbdae480722
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-08-08 18:07:09 +0000

    memcached debug

--- experiments/sg-pharma.bash
+++ experiments/sg-pharma.bash
@@ -1,1 +1,7 @@
-DATA="pharmabi
+DATA="pharmabio"
+YEAR=1999
+INITIAL_EPOCHS=200
+ANNUAL_EPOCHS=200
+NLAYERS=2 # K Hops in SG Net
+OUTFILE="results/sg-pharma.csv"
+ARGS="--n_layers $NLAYERS --weight_decay 0
