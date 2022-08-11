48553420a0b6a7be7921c428da45a6e9f9f774ef
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-08-11 09:09:54 +0000

    lodash

--- experiments/sg-pharma.bash
+++ experiments/sg-pharma.bash
@@ -4,4 +4,6 @@ INITIAL_EPOCHS=200
 ANNUAL_EPOCHS=200
 NLAYERS=2 # K Hops in SG Net
 OUTFILE="results/sg-pharma.csv"
-ARGS="--n_layers $NLAYERS --weight_decay 0
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
+hparams=("--history 1 --start cold --lr 0.
