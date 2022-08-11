27dff81e9278a7d648a5938267cf36127370cecf
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-08-11 19:18:09 +0000

    Improve build process

--- experiments/sg-pharma.bash
+++ experiments/sg-pharma.bash
@@ -6,4 +6,7 @@ NLAYERS=2 # K Hops in SG Net
 OUTFILE="results/sg-pharma.csv"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5  --rescale_lr 1. --rescale_wd 1."
 PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $INITIAL_EPOCHS"
-hparams=("--history 1 --start cold --lr 0.
+hparams=("--history 1 --start cold --lr 0.005"
+            "--history 4 --start cold --lr 0.01"
+            "--history 8 --start cold --lr 0.01"
+            "--history 21 --start cold --lr 0.
