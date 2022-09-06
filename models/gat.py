cd9303f73117e406b6a7d2a1a5c7a29a1e524894
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-09-06 18:23:39 +0000

    Update up to changes in es5-ext

--- models/gat.py
+++ models/gat.py
@@ -1,2 +1,7 @@
 """
-Graph Attention Networks in DGL using SPMV op
+Graph Attention Networks in DGL using SPMV optimization.
+References
+----------
+Paper: https://arxiv.org/abs/1710.10903
+Author's code: https://github.com/PetarV-/GAT
+Pytorch implementatio
