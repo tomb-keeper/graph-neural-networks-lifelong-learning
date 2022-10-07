30d84c99e4e1133842ab9cddf991a56fd4df601d
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-10-07 19:43:06 +0000

    Add controls

--- models/gat.py
+++ models/gat.py
@@ -35,4 +35,6 @@ class GAT(nn.Module):
             feat_drop, attn_drop, negative_slope, False, self.activation))
         # hidden layers
         for l in range(1, num_layers):
-    
+            # due to multi-head, the in_dim = num_hidden * num_heads
+            self.gat_layers.append(GATConv(
+                
