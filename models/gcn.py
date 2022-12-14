ab43e5d8b9dd2504a448bfdeab2ce24dc677f58f
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-12-14 12:45:23 +0000

    Update Homepage

--- models/gcn.py
+++ models/gcn.py
@@ -1,2 +1,5 @@
 """
-GCN implementation of the DGL library (https://dgl.ai) with minor modifica
+GCN implementation of the DGL library (https://dgl.ai) with minor modifications
+to facilitate dynamically changing graph structure.
+
+Source: https://github.com/dmlc/dgl/blob/master/examples/pytorch/gcn/gcn_mp.py
