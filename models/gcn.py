d070f139dc505d49afe898c1a56c0bd37298da28
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2022-12-14 16:58:42 +0000

    More consistent naming

--- models/gcn.py
+++ models/gcn.py
@@ -3,3 +3,10 @@ GCN implementation of the DGL library (https://dgl.ai) with minor modifications
 to facilitate dynamically changing graph structure.
 
 Source: https://github.com/dmlc/dgl/blob/master/examples/pytorch/gcn/gcn_mp.py
+"""
+import math
+import torch
+import torch.nn as nn
+
+# In DGL, GraphConv refers to Kipf's GCN Conv operator
+from dgl.nn.pytorch import 
