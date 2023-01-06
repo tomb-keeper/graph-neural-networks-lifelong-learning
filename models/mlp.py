fc3a88d207ac32f7845b366728f1ab48776c646e
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-01-06 20:24:33 +0000

    Add assertions

--- models/mlp.py
+++ models/mlp.py
@@ -8,4 +8,7 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.layers = nn.ModuleList()
 
-        sel
+        self.layers.append(nn.Linear(in_feats, n_hidden))
+        for i in range(n_layers - 1):
+            self.layers.append(nn.Linear(n_hidden, n_hidden))
+   
