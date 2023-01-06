25c5b8c49b82ff080b2acac3b1aaecab6a374aa7
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-01-06 15:23:53 +0000

    Nasreddine FAREH :  Module gestion des utilisateurs

--- models/mlp.py
+++ models/mlp.py
@@ -1,1 +1,7 @@
-""" A simple yet gene
+""" A simple yet generic MLP """
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, in_feats, n_hidden, n_classes,
+            
