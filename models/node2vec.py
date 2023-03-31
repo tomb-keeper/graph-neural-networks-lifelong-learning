ab3e912640247b3a7e7b7d2b769fe26d3a709f0b
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-03-31 14:12:18 +0000

    update de translation

--- models/node2vec.py
+++ models/node2vec.py
@@ -6,4 +6,7 @@ def add_node2vec_args(parser):
     """ Adds node2vec specific command line arguments to a given parser """
     parser.add_argument('--n2v_walk_length', type=int, default=20,
                         help="Length of node2vec random walks")
-    parser.add_argument('--n2v_walks_per_node', type=int, default=1
+    parser.add_argument('--n2v_walks_per_node', type=int, default=10,
+                        help="Number of walks per node")
+    parser.add_argument('--n2v_num_negative_samples', type=int, default=1,
+  
