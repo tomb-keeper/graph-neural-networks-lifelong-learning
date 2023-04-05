80b4c087f8f40f043bafc1220305cda6128e1604
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-04-05 08:53:01 +0000

    Split into modules

--- models/node2vec.py
+++ models/node2vec.py
@@ -11,4 +11,6 @@ def add_node2vec_args(parser):
     parser.add_argument('--n2v_num_negative_samples', type=int, default=1,
                         help="Num negative examples per positive example")
     parser.add_argument('--n2v_context_size', type=int, default=10,
-                    
+                        help="Size of sliding window within random walks")
+    parser.add_argument('--n2v_p', type=float, default=1.,
+                     
