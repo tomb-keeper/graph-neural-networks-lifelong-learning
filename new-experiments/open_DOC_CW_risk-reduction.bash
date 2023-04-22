18b8b3e6a5bca13bc538d4a950aa6987f1164ecc
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-04-22 17:53:50 +0000

    bg change

--- new-experiments/open_DOC_CW_risk-reduction.bash
+++ new-experiments/open_DOC_CW_risk-reduction.bash
@@ -4,4 +4,11 @@ ANNUAL_EPOCHS=200
 NLAYERS=1
 BACKEND="dgl"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS --backend $BACKEND"
-PRETRAIN_ARGS="--t_start $YE
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $ANNUAL_EPOCHS"
+OUTFILE="results/open_DOC_CW_risk-reduction_tau75.csv"
+
+# Exit on error
+set -e
+
+HPARAMS=(
+	"--history 1 -
