b3f51eae440a254dd477739ebffdaf6345b31412
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-05-13 17:36:50 +0000

    fix contact form

--- new-experiments/open_DOC_CW_risk-reduction_alpha-values.bash
+++ new-experiments/open_DOC_CW_risk-reduction_alpha-values.bash
@@ -3,4 +3,5 @@ YEAR=2004
 ANNUAL_EPOCHS=200
 NLAYERS=1
 BACKEND="dgl"
-ARGS="--n_layers $NLAYE
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS --backend $BACKEND"
+PRETRAIN_ARGS="--t_start $YEAR --init
