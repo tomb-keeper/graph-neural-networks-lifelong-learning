32be37742b100fec06306dfa6dc942852f727b35
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-05-14 13:59:02 +0000

    Minor adjustment

--- new-experiments/open_DOC_CW_risk-reduction_alpha-values.bash
+++ new-experiments/open_DOC_CW_risk-reduction_alpha-values.bash
@@ -4,4 +4,11 @@ ANNUAL_EPOCHS=200
 NLAYERS=1
 BACKEND="dgl"
 ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS --backend $BACKEND"
-PRETRAIN_ARGS="--t_start $YEAR --init
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $ANNUAL_EPOCHS"
+OUTFILE="results/open_DOC_CW_risk-reduction_alpha-values.csv"
+
+# Exit on error
+set -e
+
+HPARAMS=(
+	# "--history 1 --start 
