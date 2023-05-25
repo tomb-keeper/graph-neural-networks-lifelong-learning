b99dcc92cdd69a627e19ad369a308af93398988d
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-05-25 12:17:42 +0000

    Remove some code duplication

--- new-experiments/open_DOC_risk-reduction.bash
+++ new-experiments/open_DOC_risk-reduction.bash
@@ -3,4 +3,6 @@ YEAR=2004
 ANNUAL_EPOCHS=200
 NLAYERS=1
 BACKEND="dgl"
-ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --resc
+ARGS="--n_layers $NLAYERS --weight_decay 0 --dropout 0.5 --rescale_lr 1.0 --rescale_wd 1. --annual_epochs $ANNUAL_EPOCHS --backend $BACKEND"
+PRETRAIN_ARGS="--t_start $YEAR --initial_epochs $ANNUAL_EPOCHS"
+OUTFILE="results/open_DOC_risk-redu
