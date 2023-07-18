bdfe7eec182fac00f5979448193778e009ad35eb
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-07-18 18:22:07 +0000

    Оптимизированы переходы между страницами

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -3,4 +3,11 @@ import argparse
 from datasets import load_data
 import torch
 
-from lifelong_learning import make_lifelong_nodeclf_dataset, li
+from lifelong_learning import make_lifelong_nodeclf_dataset, lifelong_nodeclf_identifier
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("dataset")
+    parser.add_argument("--t_zero", default=
