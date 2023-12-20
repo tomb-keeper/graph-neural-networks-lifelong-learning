4a9495d29a4032f331796c964a520d01a427cd40
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-12-20 22:07:02 +0000

    mmm

--- scripts/compute_tdiff_dists_all_datasets.bash
+++ scripts/compute_tdiff_dists_all_datasets.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/bash 
 python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/12v-1hop data/dblp-easy
-python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/12v-2hop data/dblp-e
+python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/12v-2hop data/dblp-easy
+python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/12v-3hop data/dblp-easy
+python3 analysis/compute_tdiff_dist.py --
