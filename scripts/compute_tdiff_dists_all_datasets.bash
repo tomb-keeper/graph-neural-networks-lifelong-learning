c20e6c19ef5b2ae5deb82a211b0775a2469c9c89
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-12-20 20:02:33 +0000

    blog added

--- scripts/compute_tdiff_dists_all_datasets.bash
+++ scripts/compute_tdiff_dists_all_datasets.bash
@@ -1,2 +1,3 @@
 #!/usr/bin/bash 
-python3 analysis/compute_tdiff_dist
+python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/12v-1hop data/dblp-easy
+python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/12v-2hop data/dblp-e
