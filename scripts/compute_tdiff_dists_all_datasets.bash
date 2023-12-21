dc69eb290119e2dd30eb541ad9661f5dbfe3f086
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-12-21 05:40:55 +0000

    Updates // Auto-Sync

--- scripts/compute_tdiff_dists_all_datasets.bash
+++ scripts/compute_tdiff_dists_all_datasets.bash
@@ -2,4 +2,6 @@
 python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/12v-1hop data/dblp-easy
 python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/12v-2hop data/dblp-easy
 python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/12v-3hop data/dblp-easy
-python3 analysis/compute_tdiff_dist.py --
+python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/hard-1hop data/dblp-hard
+python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/hard-2hop data/dblp-hard
+python3 a
