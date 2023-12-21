9574f0defe005ba3891beba74ca0f4827fe35643
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-12-21 22:17:51 +0000

    Removing unused imports.

--- scripts/compute_tdiff_dists_all_datasets.bash
+++ scripts/compute_tdiff_dists_all_datasets.bash
@@ -5,4 +5,5 @@ python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/12v-3hop data/d
 python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/hard-1hop data/dblp-hard
 python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/hard-2hop data/dblp-hard
 python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/hard-3hop data/dblp-hard
-python3 analysis/compute_tdiff_dist.py --max-hops 1
+python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/pharma-1hop data/pharmabio
+python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/pharma-2hop data/phar
