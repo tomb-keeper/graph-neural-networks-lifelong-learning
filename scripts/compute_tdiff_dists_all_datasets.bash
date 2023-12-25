11171c9e77af1853db8465b25b5e4fdb3d4c718c
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2023-12-25 17:02:10 +0000

    Disable debug mode

--- scripts/compute_tdiff_dists_all_datasets.bash
+++ scripts/compute_tdiff_dists_all_datasets.bash
@@ -6,4 +6,5 @@ python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/hard-1hop data/
 python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/hard-2hop data/dblp-hard
 python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/hard-3hop data/dblp-hard
 python3 analysis/compute_tdiff_dist.py --max-hops 1 --save tdiff/pharma-1hop data/pharmabio
-python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/pharma-2hop data/phar
+python3 analysis/compute_tdiff_dist.py --max-hops 2 --save tdiff/pharma-2hop data/pharmabio
+python3 analysis/compute_tdiff_dist.py --max-hops 3 --save tdiff/pharma-3hop data/pharmabio
