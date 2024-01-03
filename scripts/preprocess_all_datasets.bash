ed5589e071af699f89c60eb7e5909d12d689aec7
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2024-01-03 12:41:49 +0000

    looking better

--- scripts/preprocess_all_datasets.bash
+++ scripts/preprocess_all_datasets.bash
@@ -2,4 +2,11 @@ set -e
 
 # Preprocess datasets 'dblp-easy', 'dblp-hard', and 'pharmabio' assuming they reside within a ./data/<dataset> directory relative to script execution cwd.
 # Important: t_zero is usually set to one task before the first evaluation task (such as t_start - 1)
-# Output will be
+# Output will be saved to ./data/<dataset>/<prepocessed_dataset>
+
+#################
+### DBLP-EASY ###
+#################
+DATASET="data/dblp-easy"
+TZERO="2003"
+echo
