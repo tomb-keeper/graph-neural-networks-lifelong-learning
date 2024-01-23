0f12aaebf0df2c25dbc2736d7b1561f470c6a461
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2024-01-23 13:34:44 +0000

    Comment changes.

--- scripts/preprocess_for_label_rate_ablation.bash
+++ scripts/preprocess_for_label_rate_ablation.bash
@@ -1,3 +1,6 @@
 set -e 
 
-OUTDIR="data/label_rate_ablatio
+OUTDIR="data/label_rate_ablation"
+BACKEND="dgl"
+
+# Preprocess datasets 'dblp-easy', 'dblp-hard', and 'pharmabio' assuming they reside within a ./data/<dataset> directory relat
