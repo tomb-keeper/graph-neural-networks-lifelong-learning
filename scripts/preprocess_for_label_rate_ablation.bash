6db6144705c22424aee27d05455c475046ab392e
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2024-01-24 16:29:03 +0000

    Fix description

--- scripts/preprocess_for_label_rate_ablation.bash
+++ scripts/preprocess_for_label_rate_ablation.bash
@@ -5,4 +5,10 @@ BACKEND="dgl"
 
 # Preprocess datasets 'dblp-easy', 'dblp-hard', and 'pharmabio' assuming they reside within a ./data/<dataset> directory relative to script execution cwd.
 # Important: t_zero is usually set to one task before the first evaluation task (such as t_start - 1)
-# Output will b
+# Output will be saved to ./data/<dataset>/<prepocessed_dataset>
+
+#################
+### DBLP-EASY ###
+#################
+# DATASET="data/dblp-easy"
+# TZERO="2
