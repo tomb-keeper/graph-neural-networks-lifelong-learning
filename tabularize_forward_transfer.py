8b7086ae0f45e30bb188b7064cb818b8705c225f
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2024-02-08 10:17:55 +0000

    minor bug fixes.

--- tabularize_forward_transfer.py
+++ tabularize_forward_transfer.py
@@ -39,3 +39,46 @@ def forward_transfer(df):
     # Average out the tasks (years)
     fwt = df.groupby(['dataset','model','history']).FWT.mean()
     return fwt
+
+# dataset,seed,model,variant,n_params,n_hidden,n_layers,dropout,history,sampling,batch_size,saint_coverage,limited_pretraining,initial_epochs,initial_lr,initial_wd,annual_epochs,annual_lr,annual_wd,
+# start,decay,year,epoch,f1_macro,accuracy
+
+for col in ['annual_lr', 'sampling', 'batch_size', 'variant']:
+    if col in df and len(df[col].unique()) > 1:
+        print(f"[warn] Don't apply this to hyperparameter optimization results! Will not group by column '{col}'")
+
+# TODO include 'seed'?
+groupby_cols = ['dataset','model','history']
+
+# Selet subset of interesting columns
+df = df[(groupby_cols + ['start', 'accuracy', 'year'])]
+# print(df.head())
+
+# print("N =", len(df))
+# print("Grouping by:", groupby_cols)
+# grouped_df = df.groupby(groupby_cols, as_index=False).accuracy.mean()
+# print(grouped_df)
+
+
+fwt = forward_transfer(df)
+
+# df = df.pivot_table(index=groupby_cols, columns='start', aggfunc=['mean', SD, SE, 'count'])
+df = df.drop('year', axis=1)
+# This aggregates accuracy grouped by dataset, model, history (index) and start (column)
+df = df.pivot_table(index=groupby_cols, columns='start', aggfunc=['mean', SE])
+
+
+# df['FWT'] = df['mean']['accuracy']['warm'] - df['mean']['accuracy']['cold']
+# print(df)
+df['FWT'] = fwt
+df['SE'] *= 1.96 
+df.rename({"SE": "1.96SE"}, inplace=True, axis=1)
+print(df * 100) 
+
+if args.save:
+    print("Saving aggregated results to:", args.save)
+    if args.latex:
+        with open(args.save, 'w') as fhandle:
+            print(df.to_latex(), file=fhandle)
+    else:
+        df.to_csv(args.save)
---
+++ test_open_learning.py
@@ -0,0 +1,7 @@
+import open_learning
+import numpy as np
+import torch
+
+def test_evaluation():
+    # Perfect prediction with no unseen
+   
