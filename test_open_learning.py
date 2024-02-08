5c5cf0631f3d28dd56f7845b89b52a7c14262a67
tomb-keeper <98840923+tomb-keeper@users.noreply.github.com>
2024-02-08 11:37:19 +0000

    Rebase master

--- test_open_learning.py
+++ test_open_learning.py
@@ -4,4 +4,7 @@ import torch
 
 def test_evaluation():
     # Perfect prediction with no unseen
-   
+    labels = torch.tensor([0,1,2,3,5,6])
+    predictions = torch.tensor([0,1,2,3,5,6])
+    unseen_classes = set()
+    reject_mask = tor
